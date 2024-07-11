542e1fd6899aa19920df5a556537364ea9d562d0
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-11 14:23:09 +0100

    feature: addGenre

--- src/genres/controllers.js
+++ src/genres/controllers.js
@@ -1,6 +1,15 @@
 const Genre = require("./model");
 
-const addGenre = () => {};
+const addGenre = async (req, res) => {
+  console.log("req.body: ", req.body);
+  try {
+    const genre = await Genre.create(req.body);
+
+    res.status(201).json({ message: "success", genre: genre });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
 
 module.exports = {
   addGenre: addGenre,
